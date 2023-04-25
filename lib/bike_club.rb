7137b7dcc599fc6e9f1a0d88d109a9886defa401
Blaine Glasgow <glasgowb@outlook.com>
2023-04-25 11:48:32 -0700

    Made sure most rides and best time returned rider objects

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -15,25 +15,69 @@ attr_accessor
     rides = {}
     @bikers.each do |biker|
       biker.rides.each do |ride, total|
-        if rides.keys.include?(biker.name)
-          rides[biker.name] << total.count
+        if rides.keys.include?(biker)
+          rides[biker] << total.count
         else 
-          rides[biker.name] = [total.count]
+          rides[biker] = [total.count]
         end
       end
     end
-    most = {}
+      most = {}
       rides.each do |rider, rides|
         most[rider] = rides.sum
       end
-      most.max
+      most.sort_by { |key, value| value }.last
+      #this returns the Biker object as key and the total number of rides as value
   end
+
+
+  ### This is same as above bit returns string of rider name as key and total rides as value
+  # def most_rides
+  #   rides = {}
+  #   @bikers.each do |biker|
+  #     biker.rides.each do |ride, total|
+  #       if rides.keys.include?(biker.name)
+  #         rides[biker.name] << total.count
+  #       else 
+  #         rides[biker.name] = [total.count]
+  #       end
+  #     end
+  #   end
+  #   most = {}
+  #     rides.each do |rider, rides|
+  #       most[rider] = rides.sum
+  #     end
+  #     most.sort_by { |key, value| value }.last
+  # end
 
   def best_time(ride)
-    
+    best_overall = {}
+    @bikers.each do |biker|
+      best_overall[biker] = biker.personal_record(ride)
+    end
+    best_overall.sort_by { |key, value| value }.first
+    #this returns the Biker object as key and the best time as value
   end
 
+### This is same as above bit returns string of rider name as key and best time as value
+  # def best_time(ride)
+  #   best_overall = {}
+  #   @bikers.each do |biker|
+  #     best_overall[biker.name] = biker.personal_record(ride)
+  #   end
+  #   best_overall.sort_by { |key, value| value }.first
+  # end
+
+
+
   def bikers_eligible(ride)
-    
+    eligible = []
+      @bikers.each do |biker|
+        if biker.acceptable_terrain.include?(ride.terrain) && biker.max_distance > ride.total_distance
+          eligible << biker
+        end
+      end
+      eligible
   end
+
 end
--- spec/bike_club.rb
+++ spec/bike_club.rb
@@ -42,35 +42,17 @@ RSpec.describe BikeClub do
       expect(@bike_club.bikers).to include(@biker, @biker2)
     end
     it 'can see who has the most rides' do
-      expect(@bike_club.most_rides).to eq(["Kenny", 4])
+      expect(@bike_club.most_rides).to eq([@biker, 4])
+      # expect(@bike_club.most_rides).to eq(["Kenny", 4])
+    end
+    it 'can show best time on a ride' do
+      expect(@bike_club.best_time(@ride2)).to eq([@biker, 60.9])
+      # expect(@bike_club.best_time(@ride2)).to eq(["Kenny", 60.9])
+    end
+    it 'can show eligible bikers' do
+      expect(@bike_club.bikers_eligible(@ride1)).to include(@biker)
+      expect(@bike_club.bikers_eligible(@ride2)).to include(@biker, @biker2)
     end
   end
 
 end
-
-
-## Iteration 3 - Bike Club
-
-# Use TDD to create a `BikeClub` class that adds the following functionality:
-
-# 1. Each `BikeClub` has a name, as well as a way to read that data
-# 1. Each `BikeClub` has a list of Bikers, and the `BikeClub` can add new Bikers.
-
-# Additionally, use TDD to add the following functionality to the `BikeClub` class. 
-# A passing challenge will complete at least one of the following. 
-# We recommend completing more than one if you have time.
-
-# 1. A `BikeClub` can tell us which Biker has logged the most rides.
-# 1. A `BikeClub` can tell us which Biker has the best time for a given Ride.
-# 1. A `BikeClub` can tell us which Bikers are eligible for a given Ride. 
-#     A Biker is eligible for a Ride if the terrain is acceptable to them and 
-#     the total distance does not exceed their max distance.
-
-# | Method Name | Return Value |
-# | ----------- | ------------ |
-# | `name` | Club name as a `String` |
-# | `bikers` | `Array` of `Biker` objects |
-# | `add_biker(biker)` | `Array` of `Biker` objects |
-# | `most_rides` | `Biker` object |
-# | `best_time(ride)` | `Biker` object |
-# | `bikers_eligible(ride)` | `Array` of `Biker` objects |
